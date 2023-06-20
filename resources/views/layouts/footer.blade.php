951ae9d376c7cbd71bad693f25dd78b0ba6b0a71
Faizan_cs <faizan@codestudio.pk>
2023-06-21 04:25:45 +0500

    FEAT: Setup Laravel Project and Admin Panel Design Partialization

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,14 @@
+<!-- Jquery Core Js -->
+<script src="{{asset('assets/bundles/libscripts.bundle.js')}}"></script> <!-- Lib Scripts Plugin Js ( jquery.v3.2.1, Bootstrap4 js) -->
+<script src="{{asset('assets/bundles/vendorscripts.bundle.js')}}"></script> <!-- slimscroll, waves Scripts Plugin Js -->
+
+<script src="{{asset('assets/bundles/jvectormap.bundle.js')}}"></script> <!-- JVectorMap Plugin Js -->
+<script src="{{asset('assets/bundles/sparkline.bundle.js')}}"></script> <!-- Sparkline Plugin Js -->
+<script src="{{asset('assets/bundles/c3.bundle.js')}}"></script>
+
+<script src="{{asset('assets/bundles/mainscripts.bundle.js')}}"></script>
+<script src="{{asset('assets/js/pages/index.js')}}"></script>
+</body>
+
+
+</html>
